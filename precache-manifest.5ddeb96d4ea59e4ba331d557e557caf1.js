053f5536ce226af533387c946b6f83b629c14117
nathan olenik <nolenik23@outlook.com>
2020-03-29 18:36:31 -0400

    test

---
+++ precache-manifest.5ddeb96d4ea59e4ba331d557e557caf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91db8031ae3fcfd5854d232011601572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d93f88f20499cb961f8e",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "86b118104baa8ddeb58c",
+    "url": "/static/js/2.cf9d7817.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf9d7817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93f88f20499cb961f8e",
+    "url": "/static/js/main.93680696.chunk.js"
+  },
+  {
+    "revision": "d1fd180e7e261fb3fdcc",
+    "url": "/static/js/runtime-main.3b1a12e0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
